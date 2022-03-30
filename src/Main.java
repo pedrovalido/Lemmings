93c3af6dbbc6bdb23f2254ae6f81a884ff9bb31d
Pedro <pedro.validex@gmail.com>
2022-03-31 00:47:30 +0100

    compileTimeErrorsInMooshakFixed

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,24 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import lemming.Lemming;
+
+/**
+ * 
+ * @author PedroValido 56760
+ *
+ */
 public class Main {
 
-	private static final int MAX_N2 = 1000;
-
+	/**
+	 * Function providing entry point to the problem resolution
+	 * 
+	 * Reads the inputs and processes it
+	 * 
+	 * @param args
+	 * @throws NumberFormatException
+	 * @throws IOException
+	 */
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
 
@@ -31,38 +45,57 @@ public class Main {
 				lemmingsSecondLine[k] = new Lemming(line[0].trim().charAt(0), Integer.parseInt(line[1]));
 			}
 
-			calc(lemmingsFirstLine, lemmingsSecondLine);
+			if (lemmingsFirstLine.length != 0 && lemmingsSecondLine.length !=0)
+				calc(lemmingsFirstLine, lemmingsSecondLine);
+			else
+				System.out.println("0 0");
 		}
 
 	}
 
+	/**
+	 * Calculates the maximum amount of points possible and minimum amount of pairs
+	 * to do so Prints the result in the default output
+	 * 
+	 * @param line1 - line of Lemmings
+	 * @param line2 - line of Lemmings
+	 */
 	private static void calc(Lemming[] line1, Lemming[] line2) {
-		int minPairs =0;
-		
-		int[][] pointMatrix = new int[line1.length][line2.length];
-		
-		//s(i, j) = 0 , i=0 || j=0
-		//s(i, j) = max( s(i-1,j), s(i, j-1), drop(l1, l2) + s(i-1,j-1) )
-		for(int i=0;i<line1.length;i++) {
-			for(int j=0;j<line2.length;j++) {
+		int minPairs = 0;
+
+		int[][] pointMatrix = new int[line1.length+1][line2.length+1];
+
+		for (int i = 0; i < line1.length+1; i++) {
+			//solucao alternativa j=i ver qual e maior e ir so de j=i ate length
+			for (int j = 0; j < line2.length+1; j++) {
 				int p = 0;
-				if(i!=0&&j!=0) {
-					int option12 = Math.max(pointMatrix[i-1][j],  pointMatrix[i][j-1]);
-					
-					int option3 = pointMatrix[i-1][j-1] + dropTwoLemmings(line1[i], line2[j]);
-					
-					p=Math.max(option12,  option3);
-					if(p==option3)minPairs++;
+				if (i != 0 && j != 0) {
+					int option12 = Math.max(pointMatrix[i - 1][j], pointMatrix[i][j - 1]);
+
+					int drop = dropTwoLemmings(line1[i-1],line2[j-1]);
+					if(drop>0)
+						minPairs++;
+					int option3 = pointMatrix[i - 1][j - 1] + drop;
+					p = Math.max(option12, option3);
 				}
 				pointMatrix[i][j] = p;
 			}
 		}
-		
-		System.out.println(pointMatrix[line1.length-1][line2.length-1] + " " + minPairs);
+
+		System.out.println(pointMatrix[line1.length][line2.length] + " " + minPairs/2);
 	}
 
+	/**
+	 * Calculates the amount of points given by the pair of lemmings in the input
+	 * 
+	 * @param l1 - Lemming
+	 * @param l2 - Lemming
+	 * @return points generated
+	 */
 	private static int dropTwoLemmings(Lemming l1, Lemming l2) {
-		return l1.tribe == l2.tribe ? l1.points + l2.points : 0;
+		if(l1.tribe == l2.tribe)
+			return l1.points + l2.points;
+		return 0;
 	}
 
 }
--- src/lemming/Lemming.java
+++ src/lemming/Lemming.java
@@ -1,8 +1,10 @@
-class Lemming {
+package lemming;
+
+public class Lemming {
 	public char tribe;
 	public int points;
 
-	Lemming(char tribe, int points) {
+	public Lemming(char tribe, int points) {
 		this.tribe = tribe;
 		this.points = points;
 	}
