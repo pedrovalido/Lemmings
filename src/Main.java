3ee5fa551c656db08322b1c8691eb3a0150c9036
Pedro <pedro.validex@gmail.com>
2022-04-02 19:35:44 +0100

    major changes

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 import lemming.Lemming;
+import point.Point;
 
 /**
  * 
@@ -45,7 +46,9 @@ public class Main {
 				lemmingsSecondLine[k] = new Lemming(line[0].trim().charAt(0), Integer.parseInt(line[1]));
 			}
 
-			if (lemmingsFirstLine.length != 0 && lemmingsSecondLine.length != 0)
+//			System.out.println("GAME1");
+
+			if (lemmingsFirstLine.length > 0 && lemmingsSecondLine.length > 0)
 				calc(lemmingsFirstLine, lemmingsSecondLine);
 			else
 				System.out.println("0 0");
@@ -61,27 +64,63 @@ public class Main {
 	 * @param line2 - line of Lemmings
 	 */
 	private static void calc(Lemming[] line1, Lemming[] line2) {
-		int minPairs = 0;
-
-		int[][] pointMatrix = new int[line1.length + 1][line2.length + 1];
+		Point[][] pointMatrix = new Point[line1.length + 1][line2.length + 1];
 
 		for (int i = 0; i < line1.length + 1; i++) {
 			for (int j = 0; j < line2.length + 1; j++) {
-				int p = 0;
+				Point p = new Point(0, 0);
 				if (i != 0 && j != 0) {
-					int option12 = Math.max(pointMatrix[i - 1][j], pointMatrix[i][j - 1]);
+					long option1 = pointMatrix[i - 1][j].points;
+
+					long option2 = pointMatrix[i][j - 1].points;
 
 					int drop = dropTwoLemmings(line1[i - 1], line2[j - 1]);
-					if (drop > 0)
-						minPairs++;
-					int option3 = pointMatrix[i - 1][j - 1] + drop;
-					p = Math.max(option12, option3);
+					long option3 = pointMatrix[i - 1][j - 1].points + dropTwoLemmings(line1[i - 1], line2[j - 1]);
+
+					if (option1 >= option2 && option1 >= option3) {
+						if (option1 == option2)
+							p = new Point(option1, Math.min(pointMatrix[i - 1][j].pairs, pointMatrix[i][j - 1].pairs));
+						else
+							p = new Point(option1, pointMatrix[i - 1][j].pairs);
+
+					}
+					if (option2 >= option1 && option2 >= option3) {
+						if (option1 == option2)
+							p = new Point(option1, Math.min(pointMatrix[i - 1][j].pairs, pointMatrix[i][j - 1].pairs));
+						else
+							p = new Point(option2, pointMatrix[i][j - 1].pairs);
+
+					}
+					if (option3 > option1 && option3 > option2) {
+//
+//						System.out.println(
+//								"antes :" + pointMatrix[i - 1][j - 1].points + " " + pointMatrix[i - 1][j - 1].pairs);
+						p = new Point(option3,
+								drop > 0 ? pointMatrix[i - 1][j - 1].pairs + 1 : pointMatrix[i - 1][j - 1].pairs);
+
+//						System.out.println("depois " + p.points + " " + p.pairs);
+					}
+
 				}
 				pointMatrix[i][j] = p;
+
+//				System.out.println();
+//				System.out.println("matrix " + i + "," + j);
+//				for (int k = 0; k < pointMatrix.length; k++) {
+//					String line = "";
+//					for (int k1 = 0; k1 < pointMatrix[k].length; k1++) {
+//						if (pointMatrix[k][k1] == null)
+//							line += "0,0 ";
+//						else
+//							line += pointMatrix[k][k1].points + "," + pointMatrix[k][k1].pairs + " ";
+//					}
+//					System.out.println(line);
+//				}
 			}
 		}
 
-		System.out.println(pointMatrix[line1.length][line2.length] + " " + minPairs / 2);
+		System.out.println(
+				pointMatrix[line1.length][line2.length].points + " " + pointMatrix[line1.length][line2.length].pairs);
 	}
 
 	/**
---
+++ src/point/Point.java
@@ -0,0 +1,12 @@
+package point;
+
+public class Point {
+	
+	public int pairs;
+	public long points;
+	
+	public Point(long points, int pairs) {
+		this.points = points;
+		this.pairs = pairs;
+	}
+}
